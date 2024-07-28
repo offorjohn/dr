5caa6f0a4ecbde10c171eab4e8bb8f1f37a62477
offor <offorchukwuebuka8@gmail.com>
2024-07-28 04:39:56 +0200

    second commit

--- run.py
+++ run.py
@@ -1,26 +1,12 @@
-import streamlit as st
-import whisper
+from fastapi import FastAPI
+import gradio as gr
 
+from gradio_ui import demo
 
+app = FastAPI()
 
+@app.get('/')
+async def root():
+    return 'Gradio app is running at /gradio', 200
 
-audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
-
-model = whisper.load_model("base")
-
-st.text("whiser Model Loaded")
-
-
-
-if st.sidebar.button("Transcribe Audio"):
-    if audio_file is not None: 
-        st.sidebar.success("Transcribing Audio")
-        transcription = model.transcribe(audio_file.name)
-        st.sidebar.success("Transcrition Complete")
-      
-        st.markdown(transcription["text"])
-    else:
-        st.sidebar.error("Please upload on audio file")
-
-st.sidebar.header("Play Original Audio File")
-st.sidebar.audio(audio_file)        
+app = gr.mount_gradio_app(app, demo, path='/gradio')

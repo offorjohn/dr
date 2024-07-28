db864e5ecd5b9649fbff2d2ce88483e814303c41
offor <offorchukwuebuka8@gmail.com>
2024-07-28 05:44:30 +0200

    second commit

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,13 +1,24 @@
+from fastapi import FastAPI
 import gradio as gr
 
-
+# Define the Gradio interface
 def greet(text: str) -> str:
     return text
-
 
 demo = gr.Interface(
     fn=greet,
     inputs=gr.components.Textbox(label='Input'),
     outputs=gr.components.Textbox(label='Output'),
-    allow_flagging='never'
+    allow_flagging='never',
+    title="My Gradio App"
 )
+
+# Create the FastAPI application
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {"message": "Gradio app is running at /gradio"}, 200
+
+# Mount the Gradio app to the FastAPI application
+app = gr.mount_gradio_app(app, demo, path='/gradio')
